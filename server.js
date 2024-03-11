46801802310282db17d7702eb0a468743515b2ac
apkallum <apkallum@protonmail.com>
2024-03-11 19:55:41 -0400

    add fusionauth

--- server.js
+++ server.js
@@ -14,14 +14,37 @@ async function printResult() {
 
 
 app.post('/', jsonParser, async (req, res) => {
-    const results = await authUser(req.body.loginId, req.body.password);
-    res.send(JSON.stringify(results, null, 2));
+    const response = await authUser(req.body.loginId, req.body.password);
+    if (response.accessToken) {
+        const fronteggUser = await getUser(response.userId)
+        const fusionUser = transformToFusionUserObject(fronteggUser)
+        res.send(JSON.stringify(fusionUser, null, 2));
+    }
+
 });
 
 app.listen(port, function () {
     console.log(`App listening on port ${port}!`);
 });
 
+function transformToFusionUserObject(fronteggUser) {
+    let epochTime = new Date(fronteggUser.createdDateTime);
+    epochTime = epochTime.getTime() / 1000;
+    let fusionUser = {
+        id: fronteggUser.id,
+        active: true,
+        fullName: fronteggUser.name,
+        email: fronteggUser.email,
+        verified: fronteggUser.verified,
+        insertInstant: epochTime,
+        data: {
+            frontegg: {
+                tenants: fronteggUser.tenants
+            }
+        }
+    }
+    return fusionUser;
+}
 
 
 
